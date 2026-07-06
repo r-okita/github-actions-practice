7fe65685fd44e721eded3ab7c4626db81516d4a5
rokita <r-okitasfk-nga.co.jp>
2026-07-06 17:03:40 +0900

    [mod] Viteプロジェクト作業環境を構築

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,22 @@
-const tsParser = require("@typescript-eslint/parser");
-const tsPlugin = require("@typescript-eslint/eslint-plugin");
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-module.exports = [
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    ignores: ["node_modules/", "dist/", "build/", "coverage/"],
-  },
-  {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
-      parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "module",
-      // tsconfig.jsonを使用した型チェックを行う場合の設定
-      // parserOptions: {
-      //   project: "./tsconfig.json",
-      // },
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-    },
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
+      globals: globals.browser,
     },
   },
-];
+])
